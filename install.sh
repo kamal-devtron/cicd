92122323b144677474830f45c7c72d0077a77708
Kamal Acharya <kamalacharya@Kamals-MacBook-Pro.local>
2023-07-30 20:22:52 +0530

    kuberentes

--- install.sh
+++ install.sh
@@ -2,5 +2,8 @@ sudo snap install microk8s --classic --channel=1.26
 echo "alias kubectl='microk8s kubectl '" >> ~/.bashrc
 echo "alias helm='microk8s helm3 '" >> ~/.bashrc
 source ~/.bashrc
+sudo usermod -a -G microk8s devtron
+sudo chown -f -R devtron ~/.kube
+newgrp microk8s
 microk8s enable dns
 microk8s enable helm3
