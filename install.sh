866fea7587dadf5d5e18e0f0194d81515ac380c8
Kamal Acharya <kamalacharya@Kamals-MacBook-Pro.local>
2023-07-31 18:58:01 +0530

    changes

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
-sudo snap install microk8s --classic --channel=1.26
-echo "alias kubectl='microk8s kubectl '" >> ~/.bashrc
-echo "alias helm='microk8s helm3 '" >> ~/.bashrc
-source ~/.bashrc
-sudo usermod -a -G microk8s ubuntu
-sudo chown -f -R ubuntu ~/.kube
-newgrp microk8s
-microk8s enable dns
-microk8s enable helm3
+sudo snap install microk8s --classic --channel=1.24
+sudo usermod -a -G microk8s $USER
+sudo chown -f -R $USER ~/.kube
+sudo su - $USER
+microk8s enable dns storage helm3
+echo "alias kubectl='microk8s kubectl '" >> .bashrc
+echo "alias helm='microk8s helm3 '" >> .bashrc
+source .bashrc
